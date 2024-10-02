3bf0077316b3f33df82a1d8f718fed9021526911
obah <olusojiobah@gmail.com>
2024-10-02 08:24:20 +0100

    feat: contract improvements and implement deploy script

--- contracts/PlayLudo.sol
+++ contracts/PlayLudo.sol
@@ -1,32 +1,33 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "./NumberGenerator.sol";
-
 contract PlayLudo {
-    error INSUFFICIENT_PLAYERS();
-
-    NumberGenerator public throwResult;
-
-    uint8 steps = 10;
+    address immutable OWNER;
+    address winner;
     address[] public players;
+
+    uint8 diceSides = 6;
+    uint8 steps = 52;
     uint8 activePlayers;
     uint8 turn;
-    address winner;
 
     struct Player {
-        uint position;
+        uint8 position;
         bool isActive;
     }
 
     mapping(address => Player) public playerInfo;
 
-    constructor(address[] memory _players) {
+    constructor() {
+        OWNER = msg.sender;
+    }
+
+    function setPlayers(address[] memory _players) external {
+        require(msg.sender == OWNER, "UNATHOURIZED_ACCESS");
         require(_players.length > 1, "INSUFFICIENT_PLAYERS");
 
         players = _players;
         activePlayers = uint8(_players.length);
-        throwResult = NumberGenerator(msg.sender);
 
         for (uint8 i = 0; i < _players.length; i++) {
             require(_players[i] != address(0), "ADDRESS_ZERO");
@@ -39,7 +40,7 @@ contract PlayLudo {
         require(msg.sender == players[turn], "WRONG_TURN");
         require(activePlayers > 1, "GAME_ENDED");
 
-        uint diceRoll = throwResult.rollDice();
+        uint diceRoll = _rollDice();
 
         movePlayer(msg.sender, diceRoll);
 
@@ -48,11 +49,11 @@ contract PlayLudo {
         return diceRoll;
     }
 
-    function movePlayer(address _player, uint _diceRoll) internal {
+    function movePlayer(address _player, uint _diceRoll) private {
         Player storage player = playerInfo[_player];
 
         if (player.isActive) {
-            player.position = (player.position + _diceRoll) % steps;
+            player.position = uint8((player.position + _diceRoll) % steps);
 
             if (player.position == 0) {
                 player.isActive = false;
@@ -62,9 +63,19 @@ contract PlayLudo {
         }
     }
 
-    function nextTurn() internal {
+    function nextTurn() private {
         do {
             turn = (turn + 1) % uint8(players.length);
         } while (!playerInfo[players[turn]].isActive);
+    }
+
+    function _rollDice() private view returns (uint8) {
+        bytes32 _randomiser = keccak256(
+            abi.encodePacked(block.timestamp, msg.sender)
+        );
+
+        uint256 _side = (uint256(_randomiser) % diceSides) + 1;
+
+        return uint8(_side);
     }
 }
---
+++ ignition/modules/PlayLudo.ts
@@ -0,0 +1,9 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const LudoModule = buildModule("LudoModule", (m) => {
+  const ludo = m.contract("PlayLudo", []);
+
+  return { ludo };
+});
+
+export default LudoModule;
